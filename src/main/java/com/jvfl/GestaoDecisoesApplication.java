9859740a752527ade5053a7d4278612e320c1462
Joao Ferreira Luz <joaoteste733@gmail.com>
2022-08-28 16:37:38 -0300

    Atualizacoes nas regras

--- src/main/java/com/jvfl/GestaoDecisoesApplication.java
+++ src/main/java/com/jvfl/GestaoDecisoesApplication.java
@@ -1,6 +1,5 @@
 package com.jvfl;
 
-import com.jvfl.gestaodecisoes.infrastructure.reposytory.CustomRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
